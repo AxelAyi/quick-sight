7e23d4f51e06e06f48ba09dcf04d104ecf840c6e
akakwel <axelakakwel@gmail.com>
2016-04-09 03:11:32 +0200

    Closures adding

--- src/app/components/feedConnectors/feedConnectors.service.js
+++ src/app/components/feedConnectors/feedConnectors.service.js
@@ -1,16 +1,18 @@
-'use strict';
+(function() {
+  'use strict';
 
-angular
-  .module('quickSight')
-  .factory('feedConnectors', feedConnectors);
+  angular
+    .module('quickSight')
+    .factory('feedConnectors', feedConnectors);
 
-feedConnectors.$inject = ['cgSociety', 'mockFeed'];
+  feedConnectors.$inject = ['cgSociety', 'mockFeed'];
 
-function feedConnectors(cgSociety, mockFeed) {
-  var service = {
-    cgSociety: cgSociety,
-    mockFeed: mockFeed
-  };
+  function feedConnectors(cgSociety, mockFeed) {
+    var service = {
+      cgSociety: cgSociety,
+      mockFeed: mockFeed
+    };
 
-  return service;
-}
+    return service;
+  }
+})();
--- src/app/components/feedConnectors/mock.service.js
+++ src/app/components/feedConnectors/mock.service.js
@@ -1,39 +1,41 @@
-'use strict';
+(function() {
+  'use strict';
 
-angular
-.module('quickSight')
-.factory('mockFeed', mockFeed);
+  angular
+    .module('quickSight')
+    .factory('mockFeed', mockFeed);
 
-mockFeed.$injector = ['$log', '$q'];
+  mockFeed.$injector = ['$log', '$q'];
 
-function mockFeed($log, $q) {
-  var service = {
-    fetch : fetch
-  };
+  function mockFeed($log, $q) {
+    var service = {
+      fetch: fetch
+    };
 
-  return service;
+    return service;
 
-  ///////////
+    ///////////
 
-  function fetch(page, size) {
-    var deferred = $q.defer();
+    function fetch(page, size) {
+      var deferred = $q.defer();
       // Mock values
       var offset = page * size;
 
       var thumbnails = [];
-      for(var i = offset; i < offset + size; i++) {
+      for (var i = offset; i < offset + size; i++) {
         thumbnails.push(getThumbnail(i));
       }
-      
+
       deferred.resolve(thumbnails);
 
-    return deferred.promise;
-  }
+      return deferred.promise;
+    }
 
-  function getThumbnail(id) {
-    return {
-      url: 'assets/images/samples/img' + id + '.jpg',
-      source: 'mock'
+    function getThumbnail(id) {
+      return {
+        url: 'assets/images/samples/img' + id + '.jpg',
+        source: 'mock'
+    };
     }
   }
-}
+})();
